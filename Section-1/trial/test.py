2310571a770e1caed2e24290d246b4599455daa3
MandyMI35 <mndalwee@gmail.com>
2024-07-07 00:41:14 +0530

    dhhsd

--- Section-1/trial/test.py
+++ Section-1/trial/test.py
@@ -1,5 +1,13 @@
-from pycipher import Rot13
-a=Rot13().encipher('defend the east wall of the castle')
-print(a)
-b=Rot13().decipher(a)
-print(b)
+# from pycipher import Rot13
+# a=Rot13().encipher('defend the east wall of the castle')
+# print(a)
+# b=Rot13().decipher(a)
+# print(b)
+
+import codecs
+#48657861646563696D616C206973206A757374206F6E65206F66207365766572616C2028696E66696E6974656C792920706F737369626C65207261646963657321
+# Example hex values
+hex_values = "48657861646563696D616C206973206A757374206F6E65206F66207365766572616C2028696E66696E6974656C792920706F737369626C65207261646963657321" 
+result_string = codecs.decode(hex_values, 'hex').decode('utf-8')
+ 
+print(result_string)
